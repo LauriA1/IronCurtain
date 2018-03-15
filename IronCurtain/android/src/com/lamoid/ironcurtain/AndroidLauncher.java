28ede368b16c9fb3fc464e1f4a1f72f84b9d77fd
LauriA1 <t5aula00@students.oamk.fi>
2018-03-15 11:27:48 +0200

    Generate LibGdx
    for intellij IDEA

---
+++ IronCurtain/android/src/com/lamoid/ironcurtain/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lamoid.ironcurtain;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lamoid.ironcurtain.IronCurtain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IronCurtain(), config);
+	}
+}
